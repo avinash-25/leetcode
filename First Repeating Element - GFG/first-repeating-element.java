3f9d6f50a56f8fe526906f558bc95a20158b3f8d
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-08-22 22:56:06 +0530

    Added solution - LeetHub

---
+++ First Repeating Element - GFG/first-repeating-element.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total count of elements
+            int n = sc.nextInt();
+
+            // creating a new array of size n
+            int arr[] = new int[n];
+
+            // inserting elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling firstRepeated method
+            // and printing the result
+            System.out.println(ob.firstRepeated(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    // Function to    return the position of the first repeating element.
+    public static int firstRepeated(int[] arr, int n) {
+    int temp[]=new int[1000000];
+    for(int i=0; i<n ;i++){
+        temp[arr[i]] = temp[arr[i]] + 1;
+    }
+      for(int i=0; i<n ;i++){
+          if(temp[arr[i]]>1)
+           return i+1;
+      }
+      return -1;
+   }
+}
