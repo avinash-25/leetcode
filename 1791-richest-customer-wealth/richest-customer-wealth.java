5aa40bdb26bf51bae3a453f971a59252a79b721c
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-11 11:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (33.26%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,41 +1,41 @@
-// class Solution {
-//     public int maximumWealth(int[][] accounts) {
-//         //person = row;
-//         //account = col;
-//         int ans = Integer.MIN_VALUE;
-//         for(int[] ints: accounts){
-//             int sum = 0;
-//             for(int anInt:ints){
-//                 sum += anInt;
-//             }
-//             if(sum>ans){
-//                 ans = sum;
-//             }
-//         }
-//         return sum;
-//     }
-// }
-
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        // person = row;
-        // account = col;
+        //person = row;
+        //account = col;
         int ans = Integer.MIN_VALUE;
-        
-        // Loop through each person (row)
-        for (int i = 0; i < accounts.length; i++) {
+        for(int[] ints: accounts){
             int sum = 0;
-            
-            // Loop through each account (column) for the current person
-            for (int j = 0; j < accounts[i].length; j++) {
-                sum += accounts[i][j]; // Add the account balance to the sum
+            for(int anInt:ints){
+                sum += anInt;
             }
-            
-            if (sum > ans) {
-                ans = sum; // Update the maximum wealth if necessary
+            if(sum>ans){
+                ans = sum;
             }
         }
-        
-        return ans; // Return the maximum wealth among all the people
+        return ans;
     }
 }
+
+// class Solution {
+//     public int maximumWealth(int[][] accounts) {
+//         // person = row;
+//         // account = col;
+//         int ans = Integer.MIN_VALUE;
+        
+//         // Loop through each person (row)
+//         for (int i = 0; i < accounts.length; i++) {
+//             int sum = 0;
+            
+//             // Loop through each account (column) for the current person
+//             for (int j = 0; j < accounts[i].length; j++) {
+//                 sum += accounts[i][j]; // Add the account balance to the sum
+//             }
+            
+//             if (sum > ans) {
+//                 ans = sum; // Update the maximum wealth if necessary
+//             }
+//         }
+        
+//         return ans; // Return the maximum wealth among all the people
+//     }
+// }
