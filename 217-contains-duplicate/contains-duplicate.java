40b2157da90d7b004ac4c233f3b8c6d46223b7b3
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-14 21:38:29 +0530

    Time: 10 ms (86.21%) | Memory: 55 MB (66.07%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,11 +1,27 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//        int len = nums.length;
+//        Arrays.sort(nums);
+//        for(int i=0;i<len-1;i++){
+//            if(nums[i] == nums[i+1])
+//              return true;
+//        }
+//        return false;
+//     }   
+// }
+
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       int len = nums.length;
-       Arrays.sort(nums);
-       for(int i=0;i<len-1;i++){
-           if(nums[i] == nums[i+1])
-             return true;
-       }
-       return false;
-    }   
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
 }
