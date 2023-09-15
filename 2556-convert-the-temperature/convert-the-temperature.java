841794a72c79b9554aa6724648e7d3fdd5bfbf06
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-15 09:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (96.79%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double[] convertTemperature(double celsius) {
+       double[] result = new double[2];
+
+       result[0] = celsius + 273.15;
+       result[1] = celsius * 1.80 + 32.00;
+
+       return result;
+    }
+}
