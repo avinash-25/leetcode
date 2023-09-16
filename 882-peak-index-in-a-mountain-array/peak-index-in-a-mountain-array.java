d2aaab3d20926a59fd2e13b896e78ad04382e76c
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-17 01:50:07 +0530

    Time: 1 ms (22.56%) | Memory: 54.9 MB (94.81%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,16 +1,13 @@
 class Solution {
     public int peakIndexInMountainArray(int[] nums) {
        int len = nums.length;
-        int[] temp = new int[len];
-        for(int i=0;i<len;i++){
-            temp[i] = nums[i];
-        }
-        Arrays.sort(temp);
-        int n = temp[len-1];
-        for(int i=0;i<len;i++){
-            if(nums[i] == n)
-              return i;
-        }
-        return 0; 
+       int max = nums[0],i=0,temp=0;
+       for(i=1;i<len;i++){
+           if(max < nums[i]){
+              max = nums[i];
+              temp = i;
+            }
+       }
+       return temp; 
     }
 }
