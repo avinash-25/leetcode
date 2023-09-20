7fbda02c60a39fabe2e56ba003c709be6e64bbbf
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-21 00:10:03 +0530

    Time: 4 ms (14.33%) | Memory: 43.2 MB (82.06%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -13,23 +13,21 @@ import java.util.HashSet;
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // Create a HashSet to store visited nodes
+
         HashSet<ListNode> visitedNodes = new HashSet<>();
 
         ListNode current = head;
 
         while (current != null) {
-            // If the current node is already in the set, a cycle is detected
+        
             if (visitedNodes.contains(current)) {
                 return true;
             }
 
-            // Otherwise, add the current node to the set and move to the next node
             visitedNodes.add(current);
             current = current.next;
         }
 
-        // No cycle was detected
         return false;
     }
 }
