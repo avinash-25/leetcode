eafd998d7c3c5be0b5a6194d694c3463d21c03da
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-20 20:14:43 +0530

    Time: 2 ms (62.90%) | Memory: 56.4 MB (99.27%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -8,69 +8,35 @@
 //  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 //  * }
 //  */
-class Solution{
-public ListNode swapNodes(ListNode head, int k) {
-    // Initialize pointers for the kth node from the beginning and the end
-    ListNode kthFromBegin = head;
-    ListNode kthFromEnd = head;
-    ListNode currentNode = head;
 
-    // Move kthFromBegin to the kth node from the beginning
-    for (int i = 1; i < k; i++) {
-        if (kthFromBegin == null) {
-            return head; // k is out of bounds
-        }
-        kthFromBegin = kthFromBegin.next;
-    }
-
-    // Move currentNode to k+1th node from the beginning
-    currentNode = kthFromBegin.next;
-
-    // Move both pointers until currentNode reaches the end
-    while (currentNode != null) {
-        kthFromEnd = kthFromEnd.next;
-        currentNode = currentNode.next;
-    }
-
-    // Swap the values of kthFromBegin and kthFromEnd
-    int temp = kthFromBegin.val;
-    kthFromBegin.val = kthFromEnd.val;
-    kthFromEnd.val = temp;
-
-    return head;
-}
-}
-
-
-
-// class Solution {
-//     public ListNode swapNodes(ListNode head, int k) {
-//         int i=1;
-//         int count=0;
-//         ListNode tempOne = new ListNode();
-//         ListNode temptwo = new ListNode();
-//         ListNode tempthree = new ListNode();
-//         tempOne = temptwo = tempthree = head;
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        int i=1;
+        int count=0;
+        ListNode tempOne = new ListNode();
+        ListNode temptwo = new ListNode();
+        ListNode tempthree = new ListNode();
+        tempOne = temptwo = tempthree = head;
         
-//         while(temptwo != null){
-//             temptwo = temptwo.next;
-//             count++;
-//         }
-//         if(count<0 || k>count)
-//            return head;
+        while(temptwo != null){
+            temptwo = temptwo.next;
+            count++;
+        }
+        if(count<0 || k>count)
+           return head;
 
-//         while(i<k){
-//             tempOne = tempOne.next;
-//             i++;
-//         }
-//         i = (count-k+1);
-//         while(i>1){
-//             tempthree = tempthree.next;
-//             i--;
-//         }
-//         i = tempOne.val;
-//         tempOne.val = tempthree.val;
-//         tempthree.val = i;
-//         return head;
-//     }
-// }
+        while(i<k){
+            tempOne = tempOne.next;
+            i++;
+        }
+        i = (count-k+1);
+        while(i>1){
+            tempthree = tempthree.next;
+            i--;
+        }
+        i = tempOne.val;
+        tempOne.val = tempthree.val;
+        tempthree.val = i;
+        return head;
+    }
+}
