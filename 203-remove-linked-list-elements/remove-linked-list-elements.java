e7cb0e6f5bf2dc6b94cba7d838ca1878f7a8b3ea
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-09-21 13:06:40 +0530

    Time: 1 ms (88.94%) | Memory: 44.8 MB (56.89%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.java
+++ 203-remove-linked-list-elements/remove-linked-list-elements.java
@@ -9,49 +9,26 @@
  * }
  */
 class Solution {
-    // public ListNode removeElements(ListNode head, int val) {
+    public ListNode removeElements(ListNode head, int val) {
     
-    //  ListNode temp = head;
-    //  ListNode prev = null;
-    //  while (head != null && head.val == val) {
-    //     head = head.next;
-    //   }
+     ListNode temp = head;
+     ListNode prev = null;
+     while (head != null && head.val == val) {
+        head = head.next;
+      }
     //  if(head == null)
     //    return head;
 
     //  if(temp.val == val)
     //    head = head.next;
 
-    //  while(temp != null && temp.next != null){
-    //      prev = temp;
-    //      if(temp.next.val == val)
-    //          prev.next = temp.next.next;
-    //       else
-    //          temp = temp.next;
-    //    }
-    //    return head;   
-    // }
-    public ListNode removeElements(ListNode head, int val) {
-    // Handle the case where the head node has the target value
-    while (head != null && head.val == val) {
-        head = head.next;
+     while(temp != null && temp.next != null){
+         prev = temp;
+         if(temp.next.val == val)
+             prev.next = temp.next.next;
+          else
+             temp = temp.next;
+       }
+       return head;   
     }
-
-    ListNode temp = head;
-    ListNode prev = null;
-
-    while (temp != null) {
-        if (temp.val == val) {
-            // Skip the current node with the target value
-            prev.next = temp.next;
-        } else {
-            // Move to the next node
-            prev = temp;
-        }
-        temp = temp.next;
-    }
-
-    return head;
-}
-
 }
