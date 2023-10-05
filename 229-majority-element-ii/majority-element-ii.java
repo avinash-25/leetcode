7260a3a9652fd1ed30d6b42403e6b0bf1dc3c3bc
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-10-05 20:56:41 +0530

    Time: 2 ms (98.52%) | Memory: 46.8 MB (38.25%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Reset counts for the candidates
+        count1 = 0;
+        count2 = 0;
+
+        // Count the occurrences of the candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        // Check if candidates appear more than ⌊ n/3 ⌋ times
+        List<Integer> result = new ArrayList<>();
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
