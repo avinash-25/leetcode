ed350deee44ec078395c2395541c9a262913aa6f
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-10-17 01:18:26 +0530

    Time: 1 ms (100.00%) | Memory: 131.6 MB (49.01%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,18 +1,3 @@
-// class Solution {
-//     public boolean increasingTriplet(int[] nums) {
-        
-//        for(int i=0;i<nums.length-2;i++){
-//            if(nums[i] < nums[i+1] && nums[i+1] < nums[i+2])
-//               return true;      
-//        }
-//        Map<Integer, Integer> map = new HashMap<>();
-//         for (int i = 0; i < nums.length; i++) {
-//             map.put(i, nums[i]);
-//         }
-
-//     }
-// }
-
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
         int first = Integer.MAX_VALUE;
@@ -20,14 +5,14 @@ public class Solution {
 
         for (int num : nums) {
             if (num <= first) {
-                first = num; // Update the first element.
+                first = num;
             } else if (num <= second) {
-                second = num; // Update the second element.
+                second = num;
             } else {
-                return true; // We found a third element that's greater than the first two.
+                return true; 
             }
         }
 
-        return false; // No increasing triplet found.
+        return false; 
     }
 }
