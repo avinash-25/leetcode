fb4345cffda66be35be206833e293795916066b9
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-11-03 08:53:30 +0530

    Time: 1 ms (79.69%) | Memory: 40.5 MB (60.89%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> is = new Stack<>();
+        Stack<StringBuilder> ss = new Stack<>();
+
+        int n = s.length(), num = 0;
+        StringBuilder str = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+            // There will be 4 types of characters --> number, [ , ], character
+
+            if(ch >= '0' && ch <= '9') {
+                num = (num * 10) + ch - '0';
+            } else if(ch == '[') {
+                ss.push(str);
+                str = new StringBuilder();
+
+                is.push(num);
+                num = 0;
+            } else if(ch == ']') {
+                StringBuilder temp = str;
+                str = ss.pop();
+                int count = is.pop();
+
+                while(count-- > 0) {
+                    str.append(temp);
+                }
+            } else {
+                str.append(ch);
+            }
+        }
+        return str.toString();
+    }
+}
