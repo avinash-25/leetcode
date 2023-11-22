d4721cb268b199d6dd40114b4b94badad4b3c9b8
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-11-22 14:45:36 +0530

    Time: 1 ms (77.90%) | Memory: 41.2 MB (8.25%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,45 +1,21 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int sPointer = s.length() - 1;
-        int tPointer = t.length() - 1;
-        int sBackspace = 0;
-        int tBackspace = 0;
+        return processString(s).equals(processString(t));
+    }
 
-        while (sPointer >= 0 || tPointer >= 0) {
-            // Find the position of the next non-backspace character in s.
-            while (sPointer >= 0 && (s.charAt(sPointer) == '#' || sBackspace > 0)) {
-                if (s.charAt(sPointer) == '#') {
-                    sBackspace++;
-                } else {
-                    sBackspace--;
+    public String processString(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (c == '#') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
                 }
-                sPointer--;
+            } else {
+                result.append(c);
             }
-
-            // Find the position of the next non-backspace character in t.
-            while (tPointer >= 0 && (t.charAt(tPointer) == '#' || tBackspace > 0)) {
-                if (t.charAt(tPointer) == '#') {
-                    tBackspace++;
-                } else {
-                    tBackspace--;
-                }
-                tPointer--;
-            }
-
-            // Compare the current characters in s and t.
-            if (sPointer >= 0 && tPointer >= 0 && s.charAt(sPointer) != t.charAt(tPointer)) {
-                return false;
-            }
-
-            // If one string has reached the beginning while the other hasn't, they are not equal.
-            if ((sPointer >= 0 && tPointer < 0) || (sPointer < 0 && tPointer >= 0)) {
-                return false;
-            }
-
-            sPointer--;
-            tPointer--;
         }
 
-        return true;
+        return result.toString();
     }
 }
