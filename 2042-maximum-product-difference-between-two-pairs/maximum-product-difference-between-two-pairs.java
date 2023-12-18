64c577db0cfe4ef0daeb452f73ce0627398567df
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-12-18 19:56:40 +0530

    Time: 7 ms (75.02%) | Memory: 44.2 MB (54.52%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
        Arrays.sort(nums);
-       return ((nums[nums.length-2] * nums[nums.length-1]) - (nums[0] * nums[1])); 
+       int len = nums.length;
+       return ((nums[len-2] * nums[len-1]) - (nums[0] * nums[1])); 
     }
 }
