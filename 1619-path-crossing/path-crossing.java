54048304cedbc5309e3719ba46fa7e693931e37d
Avinash ranjan <117587678+avinash-25@users.noreply.github.com>
2023-12-23 10:49:13 +0530

    Time: 1 ms (96.48%) | Memory: 42.1 MB (6.17%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,34 +1,34 @@
+import java.awt.Point;
 class Solution {
     public boolean isPathCrossing(String path) {
-        HashSet<String> visit = new HashSet<>();
-        visit.add("0,0");
-        int x=0,y=0;
-        for(int i=0;i<path.length();i++)
-        {
-            if(path.charAt(i)=='N')
-            {
-                y++;
+        Set<Point> visited = new HashSet<>();
+        visited.add(new Point(0, 0));
+        int x = 0, y = 0;
+
+        for (char direction : path.toCharArray()) {
+            switch (direction) {
+                case 'N':
+                    y++;
+                    break;
+                case 'S':
+                    y--;
+                    break;
+                case 'E':
+                    x++;
+                    break;
+                case 'W':
+                    x--;
+                    break;
             }
-            else if(path.charAt(i)=='S')
-            {
-                y--;
-            }
-            else if(path.charAt(i)=='E')
-            {
-                x++;
-            }
-            else
-            {
-                x--;
-            }
-            String current = x + ","+y;
-            if (visit.contains(current))
-            {
+
+            Point current = new Point(x, y);
+            if (visited.contains(current)) {
                 return true;
             }
-            visit.add(current);
+
+            visited.add(new Point(x, y));
         }
+
         return false;
-        
     }
 }
