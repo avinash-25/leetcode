3c3d17c4f5cc3c79b5f79c631b167ece5182b62b
Avinash Ranjan. <117587678+avinash-25@users.noreply.github.com>
2025-03-14 13:26:24 +0530

    Time: 43 ms (73.8%), Space: 53.2 MB (29.21%) - LeetHub

---
+++ 2732-counter/2732-counter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10);
+ * console.log(counter()); // 10
+ * console.log(counter()); // 11
+ * console.log(counter()); // 12
+ */
