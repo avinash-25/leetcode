c21b4fba328ed8aa19673d2ab290e5321cd06a14
Avinash Ranjan. <117587678+avinash-25@users.noreply.github.com>
2025-03-24 17:33:50 +0530

    Time: 2 ms (97.63%), Space: 41.9 MB (47.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || 
+                    (ch == '}' && top != '{') || 
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
