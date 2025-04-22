04b9bcdcee3a99fba862d108ae6edb0b3923bdfd
Avinash Ranjan. <117587678+avinash-25@users.noreply.github.com>
2025-04-22 09:09:22 +0530

    Time: 42 ms (55.45%), Space: 53.4 MB (82%) - LeetHub

---
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    }
+    return Object.keys(obj).length === 0;
+};
